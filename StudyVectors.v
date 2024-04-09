a940b11530bbe85973cf8b72eec886842c24f40e
Lucas <lucas.donati@etu.unige.ch>
2024-04-09 21:29:01 +0200

    ajout de preuves sur les vecteurs

--- StudyVectors.v
+++ StudyVectors.v
@@ -7,8 +7,8 @@ Require Import Coq.Arith.Arith.
 
 Lemma forall2_hd: forall {n} A (R: A->A-> Prop) x1 x2 (v1 v2: t A n), 
   Forall2 R (x1::v1) (x2::v2) -> R x1 x2.
-intros.
-inversion H. assumption.
+intros. 
+inversion_clear H. assumption.
 Qed.
 
 Lemma forall2_tl: forall {n} A (R: A->A-> Prop) x1 x2 (v1 v2: t A n),
@@ -68,6 +68,12 @@ Qed.
 
 Locate "<".
 
+Lemma jsp: 
+  forall {n} (a b: t nat n), ~(a c= b) -> Exists2 lt b a.
+Proof.
+intros. compute in H .
+Abort.
+
 Definition VecNotIncluded {n} (a b: t nat n): Prop :=
   Exists2 lt b a.
 
@@ -103,26 +109,77 @@ Qed.
 
 Check [1;2] = 1::[2].
 
-Lemma vec_inclusion_antisymm: forall {n} (a b: t nat n), a c= b /\ b c= a -> a = b.
-intros.
-compute in H. destruct H as [H0 H1].
-dependent induction H0. trivial.
-assert (x2<=x1).
-apply (forall2_hd) in H1; trivial.
-assert (x1=x2).
-apply Nat.le_antisymm; assumption.
-apply forall2_tl in H1. apply IHForall2 in H1.
-rewrite H1; rewrite H3. trivial.
+Lemma vec_inclusion_antisymm: forall {n} (a b: t nat n), a c= b /\ b c= a <-> a = b.
+split ; intros.
+- compute in H. destruct H as [H0 H1].
+  dependent induction H0. trivial.
+  assert (x2<=x1).
+  apply (forall2_hd) in H1; trivial.
+  assert (x1=x2).
+  apply Nat.le_antisymm; assumption.
+  apply forall2_tl in H1. apply IHForall2 in H1.
+  rewrite H1; rewrite H3. trivial.
+- dependent induction H.
+  split; apply vec_eq_impl_incl; trivial.
 Qed.
 
-Lemma test8: forall {n} (a b: t nat n), ~(a c= b) <-> VecNotIncluded a b.
-intros. split; intros.
-compute in H. 
-Search (_ -> False -> _).
-Check except.
-compute.
-inversion H.
-compute. compute in H.
+Lemma vec_forall2_impl_exists2 {n} {A} R (a b: t A (S n)): 
+  Forall2 R a b -> Exists2 R a b.
+Proof.
+intros.
+dependent induction H.
+apply Exists2_cons_hd.
+assumption.
+Qed.
+
+Search In.
+Check In.
+Check In_nth.
+
+Search ((~ Forall _) <-> Exists (~ _)).
+
+Lemma vec_exists2_iff_not_forall2 {n} {A} R (a b: t A (S n)):
+  Exists2 R a b <-> ~ (Forall2 (fun x y => ~ R x y) a b).
+Proof.
+split.
+- compute.
+  intros.
+  induction H.
+  -- apply forall2_hd in H0. destruct H0. trivial.
+  -- apply forall2_tl in H0. destruct IHExists2. trivial.
+- admit.
+Admitted.
+
+Search (_ <= _ -> _ < _).
+Search (_ < _ -> _ <= _).
+Search (~(_ <= _) -> _).
+
+Lemma vec_not_inclusion: forall {n} (a b: t nat (S n)), ~(a c= b) <-> a c/= b.
+intros.
+split; intros.
+- compute. compute in H.
+  apply vec_exists2_iff_not_forall2.
+  intro. destruct H.
+  apply to_list_Forall2, List.Forall2_flip in H0.
+  assert (List.Forall2 (fun x y:nat => ~ S y <= x) (to_list a) (to_list b) -> List.Forall2 (fun x y : nat => x <= y) (to_list a) (to_list b)).
+  { apply List.Forall2_impl. 
+    intros.
+    apply Arith_prebase.gt_S_le_stt.
+    apply not_le. trivial. }
+  apply to_list_Forall2, H.
+  trivial.
+- intro. compute in H.
+  apply vec_exists2_iff_not_forall2 in H.
+  destruct H.
+  compute in H0.
+  apply to_list_Forall2, List.Forall2_flip.
+  assert (List.Forall2 (fun x y:nat => x <= y) (to_list a) (to_list b) -> List.Forall2 (fun x y : nat => ~ S y <= x) (to_list a) (to_list b)).
+  { apply List.Forall2_impl.
+    intros.
+    apply Arith_prebase.gt_not_le_stt.
+    apply Arith_prebase.le_gt_S_stt. trivial. }
+  apply H, to_list_Forall2. trivial.
+Qed.
 
 Lemma test8: forall {n} (a b: t nat n), 0<n -> ~VecIncluded a b <-> VecNotIncluded a b.
 intros.
